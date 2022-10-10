5945edcd8e7ae7c8935a8dc687c279dbb39feb4a
maks <maks-xrenov@mail.ru>
2022-10-10 22:52:13 +0300

    add scanner instead of args

--- homework1/library/controller/src/main/java/Application.java
+++ homework1/library/controller/src/main/java/Application.java
@@ -1,17 +1,17 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Ошибка. Фамилия автора не введена");
-            return;
-        }
+        Scanner input = new Scanner(System.in);
+        String inputname = input.nextLine();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         FactoryLibrary.getLibrary().getBooks().forEach(book -> {
-            String fullname = book.getAuthor().getSecondName() + book.getAuthor().getFirstName();
-            if (fullname.equals(args[0])) {
+            String fullname = book.getAuthor().getSecondName() + " " + book.getAuthor().getFirstName();
+            if (fullname.equals(inputname)) {
                 System.out.println(gson.toJson(book));
             }
 
